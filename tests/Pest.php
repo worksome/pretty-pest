d449d124d611c65c03e3cce2b1f0232260b2a771
Luke Downing <lukeraymonddowning@gmail.com>
2022-03-02 14:03:26 +0000

    Initial commit

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,7 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 use Worksome\PrettyPest\Tests\SniffTestCase;
+use Worksome\PrettyPest\Tests\Support\CodeSnifferResult;
 
 uses(SniffTestCase::class)->in('Sniffs');
 
+function checkFile(string $filePath): CodeSnifferResult
+{
+    return test()->checkFile($filePath);
+}
 
+function fixFile(string $filePath): CodeSnifferResult
+{
+    return test()->fixFile($filePath);
+}
--- tests/Sniffs/Formatting/TestsAreGroupedTogetherSniffTest.php
+++ tests/Sniffs/Formatting/TestsAreGroupedTogetherSniffTest.php
@@ -1,11 +1,9 @@
 <?php
 
-it('fails when tests are not grouped together', function () {
-    $this->checkFile(__DIR__ . '/../../Stubs/Formatting/PestStructureSniff/tests_not_grouped_together.php')
-        ->assertHasErrors([9, 19]);
-});
+it('fails when tests are not grouped together')
+    ->checkFile(__DIR__ . '/../../Stubs/Formatting/PestStructureSniff/tests_not_grouped_together.php')
+    ->assertHasErrors([9, 19]);
 
-it('can fix grouping tests together', function () {
-    $this->fixFile(__DIR__ . '/../../Stubs/Formatting/PestStructureSniff/tests_not_grouped_together.php')
-        ->assertMatchesFile(__DIR__ . '/../../Stubs/Formatting/PestStructureSniff/Fixed/tests_not_grouped_together.php');
-});
+it('can fix grouping tests together')
+    ->fixFile(__DIR__ . '/../../Stubs/Formatting/PestStructureSniff/tests_not_grouped_together.php')
+    ->assertMatchesFile(__DIR__ . '/../../Stubs/Formatting/PestStructureSniff/Fixed/tests_not_grouped_together.php');
